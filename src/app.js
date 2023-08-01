2c85331bd093fe79c66cfc7528e5297ca915136a
pck <3500146682@qq.com>
2023-08-01 23:31:12 +0800

    完成 button-group 的创建

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
+import ButtonGroup from './button-group.vue'
 
 Vue.component('sh-button', Button)
 Vue.component('sh-icon', Icon)
+Vue.component('sh-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sh-button-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+  .sh-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    > .sh-button {
+      border-radius: 0;
+      &:not(:first-child) {
+        margin-left: -1px;
+      }
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
