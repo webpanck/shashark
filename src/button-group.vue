d4c9b0e3ccb7ee4e631064be2c70f348a16e9997
pck <3500146682@qq.com>
2023-08-08 23:11:23 +0800

    检测子元素是不是 button

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    mounted() {
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase()
+        if (name !== 'button') {
+          console.warn(`sh-button-group 的子元素必须 sh-button，但你写的是 ${name}`)
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
