fb7ca8f26d65301e1b44c698c909161ad90d2bcf
pck <3500146682@qq.com>
2023-08-20 22:41:49 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft: gutter/2+'px', paddingRight:gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px">
-      <slot />
-    </div>
+  <div class="col" :class="colClasses" :style="colStyle">
+    <slot />
   </div>
 </template>
 
@@ -21,6 +18,20 @@ export default {
   data() {
     return {
       gutter: 0
+    }
+  },
+  computed: {
+    colClasses() {
+      return [
+        this.span && `col-${this.span}`,
+        this.offset && `offset-${this.offset}`
+      ]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
     }
   }
 }
