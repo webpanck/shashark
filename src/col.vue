7c382825acbe48421a67abd0a9897da7e8ac3813
pck <3500146682@qq.com>
2023-08-20 22:13:29 +0800

    基本实现 row 和 col 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot />
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft: gutter/2+'px', paddingRight:gutter/2+'px'}">
+    <div style="border: 1px solid green; height: 100px">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,14 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data() {
+    return {
+      gutter: 0
     }
   }
 }
@@ -17,14 +28,17 @@ export default {
 
 <style lang="scss" scoped>
   .col {
-    height: 100px;
-    background-color: grey;
     width: 50%;
-    border: 1px solid red;
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot />
   </div>
 </template>
 
 <script>
   export default {
-
+    name: 'SharkRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted() {
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    }
   }
 </script>
 
